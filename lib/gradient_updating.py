An issue with parabolic gradient, need fixing

lib/gradient_updating.py
@@ -1,6 +1,7 @@
 import numpy as np
+from lib.heaps import FibonacciHeap, BinaryHeap
 import fibonacci_heap_mod as fhm
-from scipy.sparse.linalg import norm
+from scipy import sparse
 
 class GradientUpdateTool(object):
     """
@@ -21,60 +22,46 @@ class GradientUpdateTool(object):
 
 class BasicGradientUpdater(GradientUpdateTool):
 
-    def __init__(self, g, type = "min"):
+    def __init__(self, g):
         self.__g = g
 
+
     def get_coordinate(self):
-        if type is "min":
-            return self.__g.argmin()
-        elif type is "max":
-            return self.__g.argmax()
-        else:
-            raise Exception("Not implemented yet")
+        return self.__g.argmin()
+
 
     def update(self, delta):
         self.__g += delta
 
+
     def get(self):
         return self.__g
+
 
     def get_norm(self):
         return self.__g.norm()
 
+
 class RidgeHeapGradientUpdater(GradientUpdateTool):
 
-    def __init__(self, g):
+    def __init__(self, g, heap_type="binary"):
         self.__g = g
-        self.__g_norm = 0
-        self.__heap = fhm.Fibonacci_heap()
-        self.__g_elements = []
-
-        if max(g.shape) <= 1e8:    # dense vectors work significantly better if not blow memory
-            for i, val in enumerate(np.squeeze(g.toarray())):
-                self.__g_elements.append(self.__heap.enqueue(i, val))
-                self.__g_norm += val**2
-
+        self.__g_norm = sparse.linalg.norm(g)
+        if heap_type is "binary":
+            self.__heap = BinaryHeap(g)
+        elif heap_type is "fibonacci":
+            self.__heap = FibonacciHeap(g)
         else:
-            raise Exception("This method hasn't been tested yet on dimensions n > 10^8, sorry :(")
+            raise Exception("Wrong heap name")
 
     def update(self, delta):
-        g_elems = self.__g_elements
-        heap = self.__heap
-
         delta_arr = np.squeeze(delta.toarray())
 
         for k in delta.nonzero()[1]:
-            old_priority = g_elems[k].get_priority()
-            new_priority = old_priority + delta_arr[k]
-            if old_priority > new_priority:
-                heap.decrease_key(entry=g_elems[k], new_priority=new_priority)
-
-            else:
-                value = g_elems[k].get_value()
-                heap.decrease_key(entry=g_elems[k], new_priority=heap.min().get_priority() - 1)
-                heap.dequeue_min()
-                g_elems[k] = heap.enqueue(value=value, priority=new_priority)
-            self.__g_norm = self.__g_norm - old_priority ** 2 + new_priority ** 2
+            old_v = self.__heap.get_value(k)
+            v = old_v + delta_arr[k]
+            self.__heap.update(k, v)
+            self.__g_norm = self.__g_norm - old_v ** 2 + v ** 2
 
         self.__g += delta
 
@@ -85,7 +72,7 @@ class RidgeHeapGradientUpdater(GradientUpdateTool):
         return self.__g_norm
 
     def get_coordinate(self):
-        return self.__heap.min().get_value()
+        return self.__heap.get_min()
 
 class LassoHeapGradientUpdater(GradientUpdateTool):
 
@@ -147,20 +134,3 @@ class LassoHeapGradientUpdater(GradientUpdateTool):
         b_priority = self.__neg_heap.min().get_priority()
 
         return a if a_priority < b_priority else -b
-
-
-class DummyGradientUpdater(GradientUpdateTool):
-    def __init__(self, g):
-        self.__g = g
-
-    def get_coordinate(self):
-        return abs(self.__g).argmax()
-
-    def get(self):
-        return self.__g
-
-    def update(self, delta):
-        self.__g += delta
-
-    def get_norm(self):
-        return norm(self.__g)

lib/heaps.py
@@ -16,15 +16,19 @@ class Heap(object):
     def update(self, k, v):
         raise Exception("Not implemented yet")
 
+    def get_value(self, k):
+        raise Exception("Not implemented yet")
+
 
 class BinaryHeap(Heap):
 
     def __init__(self, data):
+        print("binary")
         N = max(data.shape)
         self.__N = N
         self.__size = 0
-        self.__idx = -1*np.ones(N)
-        self.__k = -1*np.ones(N)
+        self.__idx = -1*np.ones(N).astype(int)
+        self.__k = -1*np.ones(N).astype(int)
         self.__v = -1*np.ones(N)
 
         if sparse.issparse(data):
@@ -49,8 +53,7 @@ class BinaryHeap(Heap):
 
 
     def __siftUp(self, i):
-        v = self.__v
-        while v[i] < v[int((i-1)/2)]:
+        while self.__v[i] < self.__v[int((i-1)/2)]:
             self.__swap(i, int((i-1)/2))
             i = int((i-1)/2)
 
@@ -71,6 +74,10 @@ class BinaryHeap(Heap):
 
     def update(self, k, v):
         pos = self.__idx[k]
+        if pos == -1:
+            self.insert(k, v)
+            return 0
+
         if self.__v[pos] > v:
             self.__v[pos] = v
             self.__siftUp(pos)
@@ -92,8 +99,11 @@ class BinaryHeap(Heap):
             return self.__k[0]
         return -1
 
+    def get_value(self, k):
+        return self.__v[self.__idx[k]]
 
-class fibonnacci_heap(Heap):
+
+class FibonacciHeap(Heap):
 
     def __init__(self, data):
         self.__heap = fhm.Fibonacci_heap()
@@ -105,9 +115,17 @@ class fibonnacci_heap(Heap):
         for k, v in enumerate(data):
             self.insert(k, v)
 
+        t = 1
+
 
     def get_min(self):
-        self.__heap.min().get_value()
+        m = self.__heap.min()
+        m = m.get_value()
+        return m
+
+
+    def get_value(self, k):
+        return self.__elements[k].get_priority()
 
 
     def insert(self, k, v):
@@ -118,7 +136,7 @@ class fibonnacci_heap(Heap):
         elements = self.__elements
         heap = self.__heap
 
-        if elements[k].get_priorty() > v:
+        if elements[k].get_priority() > v:
             heap.decrease_key(entry=elements[k], new_priority=v)
         else:
             value = elements[k].get_value()

lib/optimization.py
@@ -1,7 +1,7 @@
 import timeit
 from scipy import sparse
 from scipy.sparse.linalg import norm
-from lib.gradient_updating import RidgeHeapGradientUpdater, BasicGradientUpdater, LassoHeapGradientUpdater, DummyGradientUpdater
+from lib.gradient_updating import RidgeHeapGradientUpdater, BasicGradientUpdater, LassoHeapGradientUpdater
 from lib.step_size_calculation import RidgeParabolicStepSize, ConstantStepSize, CoordParabolicStepSize
 
 
@@ -11,7 +11,7 @@ def f(x, X, y, mu):
 def g(x, X, y, mu):
     return 2*X.T.dot(X.dot(x.T) - y) + mu*x.T
 
-def noname_algorithm_ridge(X, y, mu, x0, e, k_max = 1e5, gradient_update_mode ="heap", step ="constant",
+def noname_algorithm_ridge(X, y, mu, x0, e, k_max = 1e5, gradient_update_mode ="heap", heap_type = "binary",  step ="constant",
                            history_elements = ("g_norm", "d_sparsity", "time", "f", "gamma", "f_approx", "preproc_time")):
 
     start = timeit.default_timer()
@@ -41,7 +41,7 @@ def noname_algorithm_ridge(X, y, mu, x0, e, k_max = 1e5, gradient_update_mode ="
         raise Exception("No such step size calculation mode as as %s" % step)
 
     if gradient_update_mode is "heap":
-        grad_updater = RidgeHeapGradientUpdater(g_x[0, 1:])
+        grad_updater = RidgeHeapGradientUpdater(g_x[0, 1:], heap_type = heap_type)
     elif gradient_update_mode is "simple":
         grad_updater = BasicGradientUpdater(g_x[0, 1:])
     else:
@@ -62,7 +62,7 @@ def noname_algorithm_ridge(X, y, mu, x0, e, k_max = 1e5, gradient_update_mode ="
     for i in range(1, int(k_max)):
         min_coord = grad_updater.get_coordinate() + 1
 
-        if grad_updater.get_norm() <= e*g_norm_init:
+        if False:#grad_updater.get_norm() <= e*g_norm_init:
             return z[0, 1:] * beta, "success", history
 
         x = beta*z

main.py
@@ -1,29 +1,34 @@
 from scipy import sparse
 import numpy as np
 from lib.optimization import noname_algorithm_ridge
-
-
-def f(x, X, y, mu):
-    return sparse.linalg.norm(X.dot(x.T) - y, ord='fro')**2 + mu/2*sparse.linalg.norm(x, ord='fro')**2
+from lib.heaps import FibonacciHeap
 
 
 if __name__ == "__main__":
-    n = int(1e5)
-    m = n
-    mu = 1/n
-    X = sparse.rand(m, n, density=10/n).tocsr()
-    x_true = sparse.rand(1, n, density=0.001).tocsr()
-    x_true /= x_true.sum()
-    y = X.dot(x_true.T).tocsr()
-    e_lower_lim = X.max()
-    print("%.6f%% of non-zero elements"%(100*len(X.nonzero()[1])/(n*m)))
-    print("%d non-zero elements"%(len(X.nonzero()[1])))
-    x0 = sparse.rand(1, n, density=1/n).tocsr()
-    x0 /= x0.sum()
+    n = int(1e5)  # Размерность пространства. Осторожно: при порядке n = 10^8 памяти в 8 GB может не хватать
+    m = int(1e5)
+    mu = 1  # Коэффицент регуляризации в задаче Ridge
+    X = sparse.rand(m, n,
+                    density=20 / n).tocsr()  # Матрица исходных данных Х размера [m*n], плотность = доля ненулевых эл-тов
+    row_sums = np.array(X.sum(axis=1))[:, 0]
+    row_indices, col_indices = X.nonzero()
+    X.data /= row_sums[row_indices]
+    x_true = sparse.rand(1, n, density=0.3).tocsr()  # Искомое решение
+    x_true /= x_true.sum()  # Решение живет на единичном симплексе
+    # Вектор Y ответов в регрессии искусствdенно делается всюду плотным плотным путем зашумления
+    y = X.dot(x_true.T) + sparse.rand(m, 1, density=1, format="csr") * 0.0000000001
+    print("%d non-zero elements in matrix X" % (len(X.nonzero()[1])))
+    print(x_true.count_nonzero(), "non-zero elements in x_true")
+    x0 = sparse.rand(1, n, density=1 / n).tocsr()  # Стартовая точка -- случайный угол симплекса
 
-    x, message, history = noname_algorithm_ridge(X, y, mu, x0, e=1e-3, k_max=300, step="constant", history_elements=("time", "g_norm"))
+    #heap = FibonacciHeap(x0)
+    #m = heap.get_min()
+    #print(m)
 
-    print(message)
+    x, message, history = noname_algorithm_ridge(X, y, mu, x0, e=1e-3, k_max=10, heap_type="fibonacci", step="constant",
+                                                 history_elements=("time", "g_norm"))
+
+    print(history["g_norm"])
 
     #x, message, history = noname_algorithm_ridge(X, y, mu, x0, e=1e-3, k_max=300, step="parabolic",
     #                                             history_elements=("time", "g_norm"))